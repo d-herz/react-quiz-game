a3df90fd2ae18da32eac97464e734ee119706982
Daniel Hyres <danhyres@gmail.com>
2023-01-27 13:14:38 -0500

    Added sanitizing function for cleaning up trivia data in QuestionsPage. \n Added event handler functions for the True/false buttons in Question, succesfully passed in id for use later

--- components/Question.jsx
+++ components/Question.jsx
@@ -1,21 +1,46 @@
 import React from 'react'
 
-function Question({ask, ans}) {
+function Question(props) {
 
+  //might have to elevate these button handlers
+  function answerTrue(event, id){
+    if(props.ans === "True"){
+      console.log("True, correct!", id)
+    }else{
+      console.log("Not true, wrong!")
+    }
+  }
+  function answerFalse(event, id){
+    if(props.ans === "False"){
+      console.log("False, correct!", id)
+    }else{
+      console.log("Not false, wrong!")
+    }
+  }
   
   return (
     
     <div className="question--card">
       <h2 className="question--text">
-        {ask}
+        {props.ask}
       </h2>
       <div className="question--answers">
 
         {/* <h4 className="question--answer--text">
           {ans}
         </h4> */}
-        <button className='btn btn--ans'>TRUE</button>
-        <button className='btn btn--ans'>FALSE</button>
+        <button 
+        className='btn btn--ans'
+        onClick={(event) => answerTrue(event, props.id)}
+        >
+          TRUE
+        </button>
+        <button 
+        className='btn btn--ans'
+        onClick={(event) => answerFalse(event, props.id)}
+        >
+          FALSE
+        </button>
       </div>
     </div>
 

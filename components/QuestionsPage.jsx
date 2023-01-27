1b589f3ac7ff75a8aa5691354f098ca2756d980d
Daniel Hyres <danhyres@gmail.com>
2023-01-27 12:17:55 -0500

    Elevated api  fetch and questions state initialization to app component from questionspage

--- components/QuestionsPage.jsx
+++ components/QuestionsPage.jsx
@@ -5,20 +5,20 @@ import Footer from "./Footer"
 import { nanoid } from "nanoid"
 
 
-function QuestionsPage(props) {
+function QuestionsPage({ questions }) {
 
-  const [questions, setQuestions] = React.useState([])
+  // const [questions, setQuestions] = React.useState([])
 
   //Fetch from trivia API w/ useEffect
   //using base64 encoding for now and atob() in Question card map
   //could utilize different types T/F or multi choice questions, maybe set url to a variable and have a button press dictate
-  React.useEffect(() => {
-    fetch(`https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=boolean&encode=base64`)
-      .then(res => res.json())
-      .then(data => {
-        return setQuestions(data.results)
-      })
-  }, [])
+  // React.useEffect(() => {
+  //   fetch(`https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=boolean&encode=base64`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       return setQuestions(data.results)
+  //     })
+  // }, [])
   console.log(questions)
 
   //Check Answer on button click
